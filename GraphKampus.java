cfae2798f5ce65650bed4aef66e07797dd135a31
Adyadma Renjiro <arenjiro466@gmail.com>
2026-01-09 12:14:49 +0700

    update

--- GraphKampus.java
+++ GraphKampus.java
@@ -57,7 +57,6 @@ class WeightedGraph {
 
     private void addEdgeToList(VertexNode vertex, String dest, int weight) {
         EdgeNode newNode = new EdgeNode(dest, weight);
-
         if (vertex.edgeHead == null) {
             vertex.edgeHead = newNode;
         } else {
@@ -82,6 +81,7 @@ class WeightedGraph {
         addEdgeToList(destNode, src, weight);
     }
 
+    // DFS
     public void dfs(String start) {
         HashSet<String> visited = new HashSet<>();
         ArrayList<String> hasil = new ArrayList<>();
@@ -107,18 +107,18 @@ class WeightedGraph {
         }
     }
 
+    // BFS
     public void bfs(String start) {
         HashSet<String> visited = new HashSet<>();
         Queue<String> queue = new LinkedList<>();
+        ArrayList<String> hasil = new ArrayList<>();
 
         visited.add(start);
         queue.add(start);
 
-        System.out.print("BFS dari " + start + ": ");
-
         while (!queue.isEmpty()) {
             String lokasi = queue.poll();
-            System.out.print(lokasi + " ");
+            hasil.add(lokasi);
 
             VertexNode vertex = findVertex(lokasi);
             EdgeNode edge = vertex.edgeHead;
@@ -131,15 +131,13 @@ class WeightedGraph {
                 edge = edge.next;
             }
         }
-        System.out.println();
+
+        System.out.println("BFS dari " + start + ":");
+        System.out.println(String.join(" -> ", hasil));
     }
 
+    // Dijkstra
     public void dijkstra(String start, String end) {
-
-        if (findVertex(start) == null || findVertex(end) == null) {
-            System.out.println("Lokasi tidak ditemukan!");
-            return;
-        }
 
         HashMap<String, Integer> distance = new HashMap<>();
         HashMap<String, String> previous = new HashMap<>();
@@ -150,13 +148,13 @@ class WeightedGraph {
             distance.put(current.nama, Integer.MAX_VALUE);
             current = current.next;
         }
+
         distance.put(start, 0);
 
         while (true) {
             String currentLoc = getMinDistanceVertex(distance, visited);
-            if (currentLoc == null || currentLoc.equals(end)) {
+            if (currentLoc == null || currentLoc.equals(end))
                 break;
-            }
 
             visited.add(currentLoc);
 
@@ -178,9 +176,7 @@ class WeightedGraph {
         printShortestPath(start, end, distance, previous);
     }
 
-    private String getMinDistanceVertex(HashMap<String, Integer> distance,
-                                        HashSet<String> visited) {
-
+    private String getMinDistanceVertex(HashMap<String, Integer> distance, HashSet<String> visited) {
         int min = Integer.MAX_VALUE;
         String minVertex = null;
 
@@ -194,13 +190,8 @@ class WeightedGraph {
     }
 
     private void printShortestPath(String start, String end,
-                                   HashMap<String, Integer> distance,
-                                   HashMap<String, String> previous) {
-
-        if (distance.get(end) == Integer.MAX_VALUE) {
-            System.out.println("Tidak ada jalur dari " + start + " ke " + end);
-            return;
-        }
+            HashMap<String, Integer> distance,
+            HashMap<String, String> previous) {
 
         System.out.println("\nJarak terpendek dari " + start + " ke " + end +
                 " = " + distance.get(end) + " meter");
@@ -224,6 +215,7 @@ public class GraphKampus {
 
         WeightedGraph kampus = new WeightedGraph();
 
+        // ===== NODE LOKASI =====
         kampus.addVertex("Asrama");
         kampus.addVertex("Masjid");
         kampus.addVertex("Parkiran Motor SBS");
@@ -237,16 +229,42 @@ public class GraphKampus {
         kampus.addVertex("Lapangan Hybrid");
         kampus.addVertex("SWK");
 
+        // ===== EDGE & JARAK (meter) =====
         kampus.addEdge("Asrama", "Masjid", 95);
         kampus.addEdge("Asrama", "Parkiran Motor SBS", 50);
+        kampus.addEdge("Masjid", "Parkiran Motor SBS", 50);
         kampus.addEdge("Masjid", "Lapangan Tenis", 30);
+        kampus.addEdge("Masjid", "WICO", 135);
+
+        kampus.addEdge("Parkiran Motor SBS", "Gedung SBS", 35);
+        kampus.addEdge("Parkiran Motor SBS", "Lapangan Tenis", 35);
+        kampus.addEdge("Parkiran Motor SBS", "Parkiran Motor Utama", 90);
+
+        kampus.addEdge("Gedung SBS", "WAR Stadium", 30);
+        kampus.addEdge("Gedung SBS", "Parkiran Motor Utama", 70);
+
+        kampus.addEdge("Lapangan Tenis", "Parkiran Motor Utama", 45);
+        kampus.addEdge("Lapangan Tenis", "Gedung Utama", 60);
+        kampus.addEdge("Lapangan Tenis", "WICO", 130);
+
+        kampus.addEdge("WICO", "Gedung Utama", 150);
+        kampus.addEdge("WICO", "Parkiran Mobil", 75);
+
+        kampus.addEdge("Parkiran Motor Utama", "WAR Stadium", 65);
         kampus.addEdge("Parkiran Motor Utama", "Gedung Utama", 15);
+        kampus.addEdge("Parkiran Motor Utama", "Lapangan Hybrid", 20);
+
+        kampus.addEdge("Gedung Utama", "Lapangan Hybrid", 25);
+        kampus.addEdge("Gedung Utama", "Parkiran Mobil", 85);
         kampus.addEdge("Gedung Utama", "SWK", 100);
 
+        kampus.addEdge("WAR Stadium", "Lapangan Hybrid", 55);
+        kampus.addEdge("Lapangan Hybrid", "SWK", 135);
+        kampus.addEdge("Parkiran Mobil", "SWK", 30);
+
+        // ===== TEST =====
         kampus.dfs("Asrama");
         kampus.bfs("Asrama");
         kampus.dijkstra("Asrama", "SWK");
     }
 }
-
-//update
